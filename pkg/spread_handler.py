e975039a93f4b2a018c02fd5d2bb0a847add38c9
agustindiprospero <agustin.diprospero@beetrack.com>
2020-12-06 19:53:13 -0300

    fix: change is_trunk type value

--- pkg/spread_handler.py
+++ pkg/spread_handler.py
@@ -39,6 +39,8 @@ class SpreadHandler():
                 dispatch.pop('min_delivery_time')
                 dispatch.pop('max_delivery_time')
                 dispatch.pop('beecode')
+                dispatch.update({'is_pickup': 'false'})
+                dispatch.update({'is_trunk': 'true'})
         return paris_dispatches
 
     def create_new_trunk_route(self, truck, dispatches):
