8208c16ec88fac273a45dd35b3da9d7e4584512d
agustindiprospero <agustin.diprospero@beetrack.com>
2020-12-07 10:54:19 -0300

    feature: create right homologation for creating dispatch

--- pkg/paris_handler.py
+++ pkg/paris_handler.py
@@ -68,10 +68,15 @@ class ParisHandler():
         status = self.body.get("status")
         guide_id = self.body.get("guide") 
         substatus = self.homologate_substatus()
-        payload = {
-            "status" : int(status),
-            "substatus_code" : int(substatus)
+        if substatus == True:
+            payload = {
+            "status" : int(status)
         }
+        else:
+            payload = {
+                "status" : int(status),
+                "substatus_code" : substatus
+            }
         create = BeetrackAPI(self.api_key).update_dispatch(guide_id, payload)
         print({"Beetrack Response" : create})
         return create
@@ -105,54 +110,63 @@ class ParisHandler():
         status = self.body.get("status")
         substatus_code = self.body.get("substatus")
         sc = substatus_code
-        if status == 2 and sc == "Entrega exitosa":
-            print("Substatus Homologation : En Cliente")
-            return 61
-        elif status == 3 and sc == "Sin Moradores":
-            print("Substatus Homologation : Cliente No Está")
-            return 3
-        elif status == 3 and sc == "No se encuentra direccion":
-            print("Substatus Homologation : Dirección Errónea")
-            return 2
-        elif status == 3 and (sc == "Dificultad para llegar a domicilio" or sc == "Recibe en segunda visita" or sc == "Otro tipo de problema (describir)" or sc == "Fuera de Rango" or sc == "No se encuentra direccion"):
-            print("Substatus Homologation : Motivos Transporte")
-            return 6
-        elif status == 3 and substatus_code == "Domicilio no corresponde":
-            print("Substatus Homologation : Motivos Cliente")
-            return 30
-        elif status == 3 and substatus_code == "Anulará, incompleto o cambiado":
-            print("Substatus Homologation : Expectativa")
-            return 51
-        elif status == 3 and substatus_code == "Producto dañado":
-            print("Substatus Homologation : Daño Producto")
-            return 52
-        elif status == 3 and substatus_code == "Producto No Corresponde":
-            print("Substatus Homologation : Producto No Corresponde")
-            return 53
-        elif status == 3 and substatus_code == "Dirección Errónea - Definitivo":
-            print("Substatus Homologation : Dirección Errónea - Definitivo")
-            return 92
-        elif status == 3 and substatus_code == "Cliente No Está - Definitivo":
-            print("Substatus Homologation : Cliente No Está - Definitivo")
-            return 93
-        elif status == 3 and substatus_code == "Motivos Cliente - Definitivo":
-            print("Substatus Homologation : Motivos Cliente - Definitivo")
-            return 930
-        elif status == 3 and (sc == "Motivos Transporte - Definitivo" or sc == "Robado" or sc == "Extraviado"):
-            print("Substatus Homologation : Motivos Transporte - Definitivo")
-            return 96
-        elif status == 3 and substatus_code == "Nota de Crédito":
-            print("Substatus Homologation : Nota de Crédito")
-            return 55
-        elif status == 3 and substatus_code == "Error sistémico":
-            print("Substatus Homologation : Error sistémico")
-            return 31
-        elif status == 4 and substatus_code == "Expectativa":
-            print("Substatus Homologation : Expectativa")
-            return 51
-        elif status == 4 and substatus_code == "Daño Producto":
-            print("Substatus Homologation : Daño Producto")
-            return 52
-        elif status == 4 and substatus_code == "Producto No Corresponde":
-            print("Substatus Homologation : Producto No Corresponde")
-            return 53
+        if sc == None:
+            print("Not substatus code to homologate or not homologation for the substatus code")
+            return True
+        else:
+            if status == 2 and sc == "Entrega exitosa":
+                print("Substatus Homologation : En Cliente")
+                return 61
+            elif status == 3 and sc == "Sin Moradores":
+                print("Substatus Homologation : Cliente No Está")
+                return 3
+            elif status == 3 and sc == "No se encuentra direccion":
+                print("Substatus Homologation : Dirección Errónea")
+                return 2
+            elif status == 3 and (sc == "Dificultad para llegar a domicilio" or sc == "Recibe en segunda visita" or sc == "Otro tipo de problema (describir)" or sc == "Fuera de Rango" or sc == "No se encuentra direccion"):
+                print("Substatus Homologation : Motivos Transporte")
+                return 6
+            elif status == 3 and sc == "Domicilio no corresponde":
+                print("Substatus Homologation : Motivos Cliente")
+                return 30
+            elif status == 3 and sc == "Anulará, incompleto o cambiado":
+                print("Substatus Homologation : Expectativa")
+                return 51
+            elif status == 3 and sc == "Producto dañado":
+                print("Substatus Homologation : Daño Producto")
+                return 52
+            elif status == 3 and sc == "Producto No Corresponde":
+                print("Substatus Homologation : Producto No Corresponde")
+                return 53
+            elif status == 3 and sc == "Dirección Errónea - Definitivo":
+                print("Substatus Homologation : Dirección Errónea - Definitivo")
+                return 92
+            elif status == 3 and sc == "Cliente No Está - Definitivo":
+                print("Substatus Homologation : Cliente No Está - Definitivo")
+                return 93
+            elif status == 3 and sc == "Motivos Cliente - Definitivo":
+                print("Substatus Homologation : Motivos Cliente - Definitivo")
+                return 930
+            elif status == 3 and (sc == "Motivos Transporte - Definitivo" or sc == "Robado" or sc == "Extraviado"):
+                print("Substatus Homologation : Motivos Transporte - Definitivo")
+                return 96
+            elif status == 3 and sc == "Nota de Crédito":
+                print("Substatus Homologation : Nota de Crédito")
+                return 55
+            elif status == 3 and sc == "Error sistémico":
+                print("Substatus Homologation : Error sistémico")
+                return 31
+            elif status == 4 and sc == "Expectativa":
+                print("Substatus Homologation : Expectativa")
+                return 51
+            elif status == 4 and sc == "Daño Producto":
+                print("Substatus Homologation : Daño Producto")
+                return 52
+            elif status == 4 and sc == "Producto No Corresponde":
+                print("Substatus Homologation : Producto No Corresponde")
+                return 53
+            else: 
+                print("Not substatus code to homologate or not homologation for the substatus code")
+
+    
+
