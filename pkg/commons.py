ba90b77467792400aec3e1edc28da264beaa592f
agustindiprospero <agustin.diprospero@beetrack.com>
2020-12-09 14:30:44 -0300

    fix: print

--- pkg/commons.py
+++ pkg/commons.py
@@ -10,7 +10,8 @@ def fetch_tag_value(array, tag_name):
     print(array, type(array))
     print(tag_name, type(tag_name))
     for item in array:
-        print(item)
-      if tag_name == item['name']:
-        return item['value']
+        print("item :",item)
+        print("item[name] :", item['name'])
+        if tag_name == item['name']:
+            return item['value']
     return None
