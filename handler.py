71f6288a424df1afcd8315afdb8743fe20c24bc6
agustindiprospero <agustin.diprospero@beetrack.com>
2021-02-02 19:32:48 -0300

    fix:add prints

--- handler.py
+++ handler.py
@@ -34,7 +34,7 @@ def integrate(event, context):
     print({"Paris Route Trunk Dispatches": get_trunk_dispatches})
     if get_trunk_dispatches == []:
       print("Paris trunk route does not belong to Spread or doesn't have any Spread dispatches.")
-      response_body = "Message: Trunk route does not belong to Spread or doesn't have any Spread dispatch."
+      response = "Message: Trunk route does not belong to Spread or doesn't have any Spread dispatch."
     else:
       truck_identifier = "PAR-" + body.get("truck")
       print({"Paris vehicle in Spread account" : truck_identifier})
@@ -42,7 +42,7 @@ def integrate(event, context):
       print({"Verify existence Paris truck in Spread account" : verify_spread_truck})
       create_trunk_route_on_spread = spread.create_new_trunk_route(verify_spread_truck, get_trunk_dispatches)
       print({"Response after creating a Paris trunk route in Spread" : create_trunk_route_on_spread})
-      response_body = "Message: Trunk route was created in Spread."
+      response = {"statusCode": 200, "body": "Message: Trunk route was created in Spread."}
   
   elif (resource == "dispatch" and event == "update" and account_id == int(account_id_paris) and is_trunk == True and body.get("status") == 1):
     # Get id dispatch of the dispatches of Paris and added to a tag associated to the Spread dispatches.
@@ -53,7 +53,7 @@ def integrate(event, context):
       add_dispatch_on_Spread_route = spread.add_dispatch_to_trunk_route()
       print({"Response for add dispatch in tranck route" : add_dispatch_on_Spread_route})
       if add_dispatch_on_Spread_route != None:
-        response_body = "Message: Paris dispatch added correctly."
+        response = {"statusCode": 200, "body": "Message: Paris dispatch added correctly."}
       else:
         print({"Message" : "Unable to add dispatch"})
     else:
@@ -61,17 +61,16 @@ def integrate(event, context):
       update_dispatch_id_in_spread = spread.get_id_dispatch_spread()
       print({"Response for add dispatch_id in to Spread dispatch" : update_dispatch_id_in_spread})
       if add_dispatch_on_Spread_route != None:
-        response_body = "Message: Paris dispatch_id added correctly."
+        response = {"statusCode": 200, "body": "Message: Paris dispatch_id added correctly."}
       else:
         print({"Message" : "Unable to add dispatch_id"})
       
-
   elif (resource == "dispatch" and event == "update" and account_id == int(account_id_spread) and is_trunk == True and body.get('status') != 1):
     # Update status in trunk dispatch on Paris account.
     print({"Handler If Case" : "Update Trunk Dispatch in Paris"})
     update_trunk_dispatch_on_paris = paris.update_trunk_dispatch()
     print({"Response for update trunk dispatch in Paris" : update_trunk_dispatch_on_paris})
-    response_body =  "Message: Dispatch was updated in Paris succesfully with new status."
+    response = {"statusCode": 200, "body": "Message: Dispatch was updated in Paris succesfully with new status."}
 
   elif (resource == "dispatch" and event== "update" and account_id == int(account_id_spread) and ((is_trunk == False and is_pickup == False) or (is_trunk == False and is_pickup == True))):
     # Update trunk dispatches with last mile status in Paris account
@@ -80,16 +79,13 @@ def integrate(event, context):
     if group_name == "Paris" and (body.get("status") in [1,2,3,4]):
       update_dispatch_on_paris = paris.update_dispatch()
       print({"Response for update Spread dispatch in Paris" : update_dispatch_on_paris})
-      response_body = "Message: Dispatch was updated with new status."
+      response = {"statusCode": 200, "body": "Message: Dispatch was updated with new status."}
+      
     else:
-      response_body = "Message: Resource is dispatch but event is not update or is not Paris group or status is pending. Not doing anything."
+      response = {"statusCode": 200, "body": "Message: Resource is dispatch but event is not update or is not Paris group or status is pending. Not doing anything."}
   
   else:
-    response_body = "Message: Webhook resource is not 'route' or 'dispatch'. Not doing anything"
+    response = {"statusCode": 200, "body": "Message: Webhook resource is not 'route' or 'dispatch'. Not doing anything"}
 
-  response = {
-          "statusCode": 200,
-          "body": response_body
-      }
-  print({"Response Body": response_body})
+  print({"Response": response})
   return response
