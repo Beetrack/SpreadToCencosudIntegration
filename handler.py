e33110e86ef3eaba3f9b52829ea1390d0e505e28
agustindiprospero <agustin.diprospero@beetrack.com>
2021-02-02 18:57:51 -0300

    fix:delete module prueba

--- handler.py
+++ handler.py
@@ -42,7 +42,7 @@ def integrate(event, context):
       print({"Verify existence Paris truck in Spread account" : verify_spread_truck})
       create_trunk_route_on_spread = spread.create_new_trunk_route(verify_spread_truck, get_trunk_dispatches)
       print({"Response after creating a Paris trunk route in Spread" : create_trunk_route_on_spread})
-      response_body = response_handler(create_trunk_route_on_spread, "Message: Trunk route was created in Spread")
+      response_body = "Message: Trunk route was created in Spread."
   
   elif (resource == "dispatch" and event == "update" and account_id == int(account_id_paris) and is_trunk == True and body.get("status") == 1):
     # Get id dispatch of the dispatches of Paris and added to a tag associated to the Spread dispatches.
@@ -51,26 +51,30 @@ def integrate(event, context):
     if verify_dispatch_existance == False:
       print({"Handler If Case" : "Add Paris dispatch in clone trunk route in Spread"})
       add_dispatch_on_Spread_route = spread.add_dispatch_to_trunk_route()
+      print({"Response for add dispatch in tranck route" : add_dispatch_on_Spread_route})
+      response_body = "Message: Paris dispatch added correctly."
     else:
       print({"Handler If Case" : "Adding dispatch_id to Spread dispatch"})
       update_dispatch_id_in_spread = spread.get_id_dispatch_spread()
-    response_body = "ahora si pasa"
+      print({"Response for add dispatch_id in to Spread dispatch" : update_dispatch_id_in_spread})
+      response_body = "Message: Paris dispatch_id added correctly."
       
 
   elif (resource == "dispatch" and event == "update" and account_id == int(account_id_spread) and is_trunk == True and body.get('status') != 1):
     # Update status in trunk dispatch on Paris account.
     print({"Handler If Case" : "Update Trunk Dispatch in Paris"})
     update_trunk_dispatch_on_paris = paris.update_trunk_dispatch()
-    response_body = response_handler(update_trunk_dispatch_on_paris, "Message: Dispatch was updated in Paris succesfully with new status")
+    print({"Response for update trunk dispatch in Paris" : update_trunk_dispatch_on_paris})
+    response_body =  "Message: Dispatch was updated in Paris succesfully with new status."
 
   elif (resource == "dispatch" and event== "update" and account_id == int(account_id_spread) and ((is_trunk == False and is_pickup == False) or (is_trunk == False and is_pickup == True))):
     # Update trunk dispatches with last mile status in Paris account
     print({"Handler If Case" : "Update Spraed dispatches in Paris"})
     group_name = fetch_tag(body.get("groups"), "name")
     if group_name == "Paris" and (body.get("status") in [1,2,3,4]):
-      #Cambiar a Paris cuando se metan las credenciales de Spread
       update_dispatch_on_paris = paris.update_dispatch()
-      response_body = response_handler(update_dispatch_on_paris, "Message: Dispatch was updated with new status")
+      print({"Response for update Spread dispatch in Paris" : update_dispatch_on_paris})
+      response_body = "Message: Dispatch was updated with new status."
     else:
       response_body = "Message: Resource is dispatch but event is not update or is not Paris group or status is pending. Not doing anything."
   
