976df34c47b975f53843a4f162c0307842838e9c
agustindiprospero <agustin.diprospero@beetrack.com>
2021-02-03 21:41:57 -0300

    fix: print

--- pkg/spread_handler.py
+++ pkg/spread_handler.py
@@ -92,7 +92,7 @@ class SpreadHandler():
         tags[0]["id_dispatch_paris"] = id_dispatch
         print(self.body.get('is_pickup'))
         if self.body.get('is_pickup') == True:
-            tags('tags')[0]["pick_up"] = "True"
+            tags[0]["pick_up"] = "True"
         else:
             pass
         payload = {
