1e78cb6cf9de8069cfa7ee2df886713a6cca814c
agustin.dipropsero@beetrack.com <72219650+agustindiprospero@users.noreply.github.com>
2021-02-17 17:37:40 -0300

    fix: print spread_handler

--- pkg/spread_handler.py
+++ pkg/spread_handler.py
@@ -142,7 +142,7 @@ class SpreadHandler():
     def add_dispatch_to_trunk_route(self):
         paris_route_id = self.body.get('route_id')
         spread_route_id = self.connection.get(str(paris_route_id))
-        print("  {Get Spread Route Redis Response" : spread_route_id})
+        print({"   Get Spread Route Redis Response" : spread_route_id})
         if spread_route_id != False:
             print({"  Case" : "Add Paris dispatch in clone trunk route"})
             self.body.update({'route_id' : spread_route_id.decode('ascii')})
